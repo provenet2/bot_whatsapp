ee5f0ee6e7e69f8a72c6de8bcfa5cba45f780cc6
provenet2 <alvertina1704@gmail.com>
2024-08-22 12:21:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
+});
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+    console.log('Escanea el código QR con tu aplicación de WhatsApp.');
+});
+
+client.on('authenticated', () => {
+    console.log('Autenticado con éxito.');
+});
+
+client.on('ready', () => {
+    console.log('El cliente está listo.');
+});
+
+client.on('message', message => {
+    if (message.body.toLowerCase() === 'hola') {
+        message.reply('Hola, soy un bot de WhatsApp.');
+    }
+});
+
+client.initialize();
 
